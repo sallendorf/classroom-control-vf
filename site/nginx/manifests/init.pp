14163eb0d532b3a90db20e45699ddce0c8b3b8ff
sallendorf <scott-allendorf@uiowa.edu>
2016-06-29 14:51:48 -0500

    Refactor nginx code

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,32 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+
   package {'nginx':
     ensure => present,
   }
   
-  file {'/var/www':
+  file {['/var/www', '/etc/nginx/conf.d']:
     ensure  => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   
   file {'/var/www/index.html':
     ensure  => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   file {'/etc/nginx/nginx.conf':
     ensure  => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   
-  file {'/etc/nginx/conf.d':
-    ensure  => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-  
   file {'/etc/nginx/conf.d/default.conf':
     ensure  => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
