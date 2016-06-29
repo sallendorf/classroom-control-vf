c7d34de02edfa5d175a3302c4b567269e902fd6a
sallendorf <scott-allendorf@uiowa.edu>
2016-06-28 21:14:18 -0500

    Cleanup

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,18 +50,9 @@ node default {
 #     mode    => '0644',
 #     content => "Think before you type\n",
 #  }
-    
 #  exec { 'cowsay':
 #    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
 #    path => '/usr/local/bin',
 #    creates => '/etc/motd',
 #  }
-
-  host { 'testing.puppetlabs.vm':
-    ensure => present,
-    ip   => '127.0.0.1',
-  }
-  
-  include users
-  include skeleton
 }
