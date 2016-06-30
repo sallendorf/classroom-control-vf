1bbe373e7ea31490ddf8ae6aeeba2a709607ff98
sallendorf <scott-allendorf@uiowa.edu>
2016-06-29 22:45:30 -0500

    Manage .ssh and public_html directories

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -5,9 +5,15 @@ define users::managed_user (
     ensure => present,
   }
 
-  file { "/home/${title}":
+  file { ["/home/${title}", "/home/${title}/public_html"]:
     ensure => directory,
     owner => $title,
     group => $group,
+  }
+    file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+    mode => '0700',
   }
 }
